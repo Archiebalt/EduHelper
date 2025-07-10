63b778def6ad8514ba8c14b27fc3154755263d71
Arthur Yakovlev <arturykovlev17@gmail.com>
2025-07-10 13:05:10 +0300

    add dependencies

---
+++ src/test/java/ie/arch/tutorbot/TelegramTutorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.arch.tutorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramTutorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cbe8963fc715857c03d47a54e3f01a25c6f61f63
Arthur Yakovlev <arturykovlev17@gmail.com>
2025-07-11 12:35:25 +0300

    add telegram API connection

---
+++ src/main/java/ie/arch/tutorbot/telegram/TelegramProperties.java
@@ -0,0 +1,22 @@
+package ie.arch.tutorbot.telegram;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties("telegram-bot")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TelegramProperties {
+
+    String username;
+    String token;
+    String path;
+
+}

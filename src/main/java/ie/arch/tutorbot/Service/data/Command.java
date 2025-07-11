001da6296a0881b2cc068236ecfe4f043d562d9b
Arthur Yakovlev <arturykovlev17@gmail.com>
2025-07-11 13:06:36 +0300

    feat: add commands handler
    implementation of three commands: start, feedback, help

---
+++ src/main/java/ie/arch/tutorbot/Service/data/Command.java
@@ -0,0 +1,10 @@
+package ie.arch.tutorbot.Service.data;
+
+public class Command {
+
+    public static final String START = "/start";
+    public static final String FEEDBACK = "/feedback";
+    public static final String HELP = "/help";
+
+
+}
--- src/main/java/ie/arch/tutorbot/Service/handler/CommandHandler.java
+++ src/main/java/ie/arch/tutorbot/Service/handler/CommandHandler.java
@@ -1,31 +1,93 @@
 package ie.arch.tutorbot.Service.handler;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 import ie.arch.tutorbot.telegram.Bot;
+import ie.arch.tutorbot.telegram.TelegramProperties;
+import static ie.arch.tutorbot.Service.data.Command.*;
 
 @Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 public class CommandHandler {
+
+    TelegramProperties telegramProperties;
 
     public BotApiMethod<?> answer(Message message, Bot bot) {
 
         String command = message.getText();
 
         switch (command) {
-            case "/start" -> {
+            case START -> {
                 return start(message);
+            }
+
+            case FEEDBACK -> {
+                return feedback(message);
+            }
+
+            case HELP -> {
+                return help(message);
+            }
+
+            default -> {
+                return defaultAnswer(message);
             }
         }
 
-        return null;
+    }
 
+    private BotApiMethod<?> defaultAnswer(Message message) {
+        return SendMessage
+                .builder()
+                .chatId(message.getChatId())
+                .text("Неподдерживаемая команда")
+                .build();
+    }
+
+    private BotApiMethod<?> help(Message message) {
+        return SendMessage
+                .builder()
+                .chatId(message.getChatId())
+                .text("""
+
+                        📍 Доступные команды:
+                        - start
+                        - help
+                        - feedback
+
+                        📍 Доступные функции:
+                        - Расписание
+                        - Домашнее задание
+                        - Контроль успеваемости
+
+                                                """)
+                .build();
+    }
+
+    private BotApiMethod<?> feedback(Message message) {
+        return SendMessage
+                .builder()
+                .chatId(message.getChatId())
+                .text("""
+
+                        📍 Ссылки для обратной связи
+                        GitHub - https://github.com/Archiebalt
+                        Telegram - https://t.me/Archie1810
+
+                                                            """)
+                .disableWebPagePreview(true)
+                .build();
     }
 
     private BotApiMethod<?> start(Message message) {
-
         return SendMessage
                 .builder()
                 .chatId(message.getChatId())
@@ -38,10 +100,9 @@ public class CommandHandler {
                         📌 Прикреплять домашние задания
                         📌 Ввести контроль успеваемости
 
-                    """)
+                                """)
 
                 .build();
-    
     }
 
 }

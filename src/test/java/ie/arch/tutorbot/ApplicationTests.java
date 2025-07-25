6386ebb1130970def1c4da04238d9c4aec369010
Arthur Yakovlev <arturykovlev17@gmail.com>
2025-07-25 15:32:59 +0300

    test: add tests

---
+++ src/test/java/ie/arch/tutorbot/ApplicationTests.java
@@ -0,0 +1,14 @@
+package ie.arch.tutorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    void contextLoad() {
+
+    }
+
+}
